b7ffea851728e849180ad4ea3053578ecd563984
Mohamedshaban11 <mohamedwoody11@gmail.com>
2023-07-06 13:58:32 +0200

    first commit

---
+++ src/components/AllinOne.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { CheckIcon } from "@heroicons/react/outline";
+
+const AllinOne = () => {
+  return (
+    <div className=" w-full my-40">
+      <div name='platforms' className=" max-w-[1240px] mx-auto px-2">
+        <h2 className=" text-5xl font-bold text-center">All-In-One Platform</h2>
+        <p className=" text-2xl py-8 text-gray-500 text-center">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+          consequuntur aspernatur harum incidunt Amet exercitationem quas iste
+          odit, eveniet quaerat voluptatum?
+        </p>
+        <div className=" grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+          <div className=" flex">
+            <div>
+              <CheckIcon className=" w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className=" font-bold text-lg">Notifications</h3>
+              <p className=" pt-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                ex odio id veni nesciunt ut consectetur quasi tempore nobis quod
+                expedita porro.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllinOne;
